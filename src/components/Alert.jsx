20f6735af6688d7f814ebcfecfa4e2c711612d22
Pacharadanai <pacharadanaipinake@gmail.com>
2023-12-20 17:02:39 +0700

    feat: add completed exercise

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,44 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert(props) {
+  let backgroundColor;
+  let icon;
+
+  if (props.type === "error") {
+    backgroundColor = "#f9c8c8";
+    icon = "/src/assets/frown.svg";
+  } else if (props.type === "warning") {
+    backgroundColor = "#F9D9C8";
+    icon = "/src/assets/alert-triangle.svg";
+  } else if (props.type === "info") {
+    backgroundColor = "#F9EBC8";
+    icon = "/src/assets/alert-circle.svg";
+  } else if (props.type === "success") {
+    backgroundColor = "#CEF7CD";
+    icon = "/src/assets/check-circle.svg";
+  }
+
+  return (
+    <div
+      css={css`
+        width: 650px;
+        display: flex;
+        font-size: 20px;
+        font-weight: bold;
+        padding: 0.4rem 1.5rem;
+        margin-top: 2rem;
+        border-radius: 10px;
+        gap: 1rem;
+        color: black;
+        background-color: ${backgroundColor};
+      `}
+    >
+      <img src={icon} alt="" />
+      <p>This is {props.type} alert box</p>
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,38 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "Primary") {
+    return (
+      <button
+        css={css`
+          padding: 1rem 4rem;
+          margin-right: 1rem;
+          background-color: hsl(221, 94%, 47%);
+          font-size: 1rem;
+          border-radius: 4px;
+          color: white;
+        `}
+      >
+        Large
+      </button>
+    );
+  } else if (props.type === "Secondary") {
+    return (
+      <button
+        css={css`
+          padding: 1rem 4rem;
+          background-color: hsl(198, 94%, 47%);
+          font-size: 1rem;
+          border-radius: 4px;
+          color: white;
+        `}
+      >
+        Large
+      </button>
+    );
+  }
+}
+
+export default Button;
